86159773afcfd8b962d65624dd91426b31ce191b
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-01 12:13:31 +0200

    Implemented cookie service

---
+++ KulinarkaBackend/Services/CookieService.cs
@@ -0,0 +1,22 @@
+﻿namespace Kulinarka.Services
+{
+    public class CookieService
+    {
+        public static void SetCookie(HttpContext httpContext, string key, string value, int? daysValid)
+        {
+            CookieOptions option = new CookieOptions();
+            option.Expires = daysValid.HasValue ? DateTime.Now.AddDays(daysValid.Value) : DateTime.Now.AddYears(5);
+            httpContext.Response.Cookies.Append(key, value, option);
+        }
+        public static string GetCookie(HttpContext httpContext, string key)
+        {
+            return httpContext.Request.Cookies[key];
+        }
+        public static void RemoveCookie(HttpContext httpContext, string key)
+        {
+            httpContext.Response.Cookies.Delete(key);
+        }
+
+
+    }
+}
