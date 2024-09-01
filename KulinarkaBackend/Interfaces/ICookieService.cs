4b2c7aaad616c2d74cf84df946bca21ebcb0633d
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-02 01:30:13 +0200

    Added interfaces for session,login,cookie service

---
+++ KulinarkaBackend/Interfaces/ICookieService.cs
@@ -0,0 +1,10 @@
+﻿namespace Kulinarka.Interfaces
+{
+    public interface ICookieService
+    {
+        void SetCookie(HttpContext httpContext, string key, string value, int yearsValid=5);
+        string GetCookie(HttpContext httpContext, string key);
+        void RemoveCookie(HttpContext httpContext, string key);
+        void SetLoginCookie(HttpContext httpContext, string username, int yearsValid=5);
+    }
+}
