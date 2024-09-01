946105b9f3ff6138bd356f7819c38c6e5d52b6b5
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-02 01:30:58 +0200

    Implemented logic for login with cookie

---
+++ KulinarkaBackend/Services/LoginService.cs
@@ -0,0 +1,44 @@
+﻿using Kulinarka.Interfaces;
+using Kulinarka.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Kulinarka.Services
+{
+    public class LoginService: ILoginService
+    {
+        private readonly DbContextTest dbContext;
+        private readonly HttpContext context;
+        private readonly ICookieService cookieService;
+        private readonly ISessionService sessionService;
+        public LoginService(DbContextTest dbContext,IHttpContextAccessor httpContextAccessor,ICookieService cookieService,ISessionService sessionService)
+        {
+            this.dbContext = dbContext;
+            this.context = httpContextAccessor.HttpContext;
+            this.cookieService = cookieService;
+            this.sessionService = sessionService;
+
+        }
+        public async Task<bool> IsLoggedInAsync()
+        {
+            string cookie = cookieService.GetCookie(context, CookieService._loginCookie);
+            User user= sessionService.GetSession<User>(context, SessionService.loginSession);
+            if (user == null && cookie == null)
+                return false;
+            if (user == null)
+                await LogInWithCookieAsync(cookie);
+            return true;
+        }
+        public async Task LogInWithCookieAsync(string username)
+        {
+            Debug.WriteLine("KRAAALJ");
+            User user = await dbContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+            sessionService.SetSession<User>(context, SessionService.loginSession,user);
+        }
+
+    }
+}
