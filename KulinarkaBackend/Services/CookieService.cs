74f8aed43d3f0f18d9a7a01ad7f82842705ed365
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-02 01:29:37 +0200

    Changed logic in cookie service and added session service

--- KulinarkaBackend/Services/CookieService.cs
+++ KulinarkaBackend/Services/CookieService.cs
@@ -1,23 +1,29 @@
-﻿namespace Kulinarka.Services
+﻿using Kulinarka.Interfaces;
+using System.Diagnostics;
+
+namespace Kulinarka.Services
 {
-    public class CookieService
+    public class CookieService: ICookieService
     {
         public  const  string _loginCookie = "Credentials";
-        public static void SetCookie(HttpContext httpContext,string key,string value,int? daysValid)
+        public  void SetCookie(HttpContext httpContext,string key,string value,int yearsValid=5)
         {
             CookieOptions option = new CookieOptions();
-            option.Expires = daysValid.HasValue ? DateTime.Now.AddDays(daysValid.Value) : DateTime.Now.AddYears(5);
-             httpContext.Response.Cookies.Append(key, value, option);
+            option.Expires = DateTime.Now.AddYears(yearsValid);
+            httpContext.Response.Cookies.Append(key, value, option);
         }
-        public static string GetCookie(HttpContext httpContext, string key)
+        public  string GetCookie(HttpContext httpContext, string key)
         {
             return httpContext.Request.Cookies[key];
         }
-        public static void RemoveCookie(HttpContext httpContext, string key)
+        public  void RemoveCookie(HttpContext httpContext, string key)
         {
             httpContext.Response.Cookies.Delete(key);
         }
 
+        public  void SetLoginCookie(HttpContext httpContext, string username, int yearsValid=5) {
+            SetCookie(httpContext, _loginCookie, username, yearsValid);
+        } 
 
     }
 }
---
+++ KulinarkaBackend/Services/SessionService.cs
@@ -0,0 +1,25 @@
+﻿using Kulinarka.Interfaces;
+using Newtonsoft.Json;
+
+namespace Kulinarka.Services
+{
+    public class SessionService : ISessionService
+    {
+        public const string loginSession = "User";
+        public  void SetSession<T>(HttpContext httpContext, string key, T value)
+        {
+            var jsonString = JsonConvert.SerializeObject(value);
+            httpContext.Session.SetString(key, jsonString);
+        }
+        public  T GetSession<T>(HttpContext httpContext, string key)
+        {
+            var jsonString = httpContext.Session.GetString(key);
+            return jsonString != null ? JsonConvert.DeserializeObject<T>(jsonString) : default;
+        }
+
+        public  void RemoveSession(HttpContext httpContext, string key)
+        {
+            httpContext.Session.Remove(key);
+        }
+    }
+}
