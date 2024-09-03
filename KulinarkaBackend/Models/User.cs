08d23024e1d0fce1e64c6e6fdd679ceecd623caf
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-03 18:18:49 +0200

    Added user model

--- KulinarkaBackend/Models/User.cs
+++ KulinarkaBackend/Models/User.cs
@@ -1,10 +1,51 @@
-﻿namespace Kulinarka.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kulinarka.Models
 {
+    public enum Gender
+    {
+        Male = 0,
+        Female = 1
+    }
     public class User
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public DateOnly Birthday { get; set; }
+
+        [MaxLength(100)]
+        public string Location { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Password { get; set; }
+
+        public DateTime DateOfCreation { get; set; }
+
+        public string Bio { get; set; }
+
+        public byte[] Picture { get; set; }
     }
 }
