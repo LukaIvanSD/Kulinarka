899f8fc1354fb44d73c30bf65e7c7dc893f4e34b
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-06 00:05:40 +0200

    Added Response class

--- KulinarkaBackend/Models/Responses/Response.cs
+++ KulinarkaBackend/Models/Responses/Response.cs
@@ -1,19 +1,28 @@
 ﻿namespace Kulinarka.Models.Responses
 {
+    public enum StatusCode{
+        OK=200,
+        Created=201,
+        BadRequest=400,
+        Unauthorized=401,
+        NotFound=404,
+        InternalServerError=500
+    }
     public class Response<T>
     {
         public bool IsSuccess { get; set; }
         public string ErrorMessage { get; set; }
+        public StatusCode StatusCode { get; set; }
         public T Data { get; set; }
 
-        public static Response<T> Success(T data)
+        public static Response<T> Success(T data, StatusCode statusCode)
         {
-            return new Response<T> { IsSuccess = true, Data = data };
+            return new Response<T> { IsSuccess = true, Data = data,StatusCode=statusCode };
         }
 
-        public static Response<T> Failure(string errorMessage)
+        public static Response<T> Failure(string errorMessage, StatusCode statusCode)
         {
-            return new Response<T> { IsSuccess = false, ErrorMessage = errorMessage };
+            return new Response<T> { IsSuccess = false, ErrorMessage = errorMessage, StatusCode=statusCode};
         }
     }
 }
