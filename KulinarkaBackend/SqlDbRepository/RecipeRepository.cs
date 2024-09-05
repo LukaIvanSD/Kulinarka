2b2d9356e34a688126ab7bd82bc66cf962348c0c
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-06 00:07:38 +0200

    Added repositories for recipe and user and generic repository for crud functions

---
+++ KulinarkaBackend/SqlDbRepository/RecipeRepository.cs
@@ -0,0 +1,42 @@
+﻿using Kulinarka.Models;
+using Kulinarka.Models.Responses;
+using Kulinarka.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kulinarka.SqlDbRepository
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        private readonly DbSet<Recipe> dbSet;
+        private readonly IRepository<Recipe> repository;
+        public RecipeRepository(AppDbContext context,IRepository<Recipe> repository)
+        {
+            this.dbSet = context.Recipes;
+            this.repository = repository;
+        }
+        public Task<Response<Recipe>> CreateAsync(Recipe recipe)
+        {
+            return repository.CreateAsync(recipe);
+        }
+
+        public Task<Response<Recipe>> DeleteAsync(int id)
+        {
+            return repository.DeleteAsync(id);
+        }
+
+        public Task<Response<List<Recipe>>> GetAllAsync()
+        {
+            return repository.GetAllAsync();
+        }
+
+        public Task<Response<Recipe>> GetByIdAsync(int id)
+        {
+            return repository.GetByIdAsync(id);
+        }
+
+        public Task<Response<Recipe>> UpdateAsync(int id, Recipe entity)
+        {
+            return repository.UpdateAsync(id, entity);
+        }
+    }
+}
