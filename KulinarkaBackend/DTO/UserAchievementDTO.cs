27b4615b26e1613b0224fb8940715b41e47c3941
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-07 14:20:48 +0200

    added automapper and implemented on achivementDto

---
+++ KulinarkaBackend/DTO/UserAchievementDTO.cs
@@ -0,0 +1,21 @@
+﻿using Kulinarka.Models;
+
+namespace Kulinarka.DTO
+{
+    public class UserAchievementDTO
+    {
+        public Achievement Achievement { get; set; }
+        public int PointsCollected { get; set; }
+        public DateTime? AquiredDate { get; set; }
+        public UserAchievementDTO(UserAchievement userAchievement)
+        {
+            Achievement = userAchievement.Achievement;
+            PointsCollected = userAchievement.PointsCollected;
+            AquiredDate = userAchievement.AquiredDate;
+        }
+        public UserAchievementDTO()
+        {
+        }
+
+    }
+}
---
+++ KulinarkaBackend/Profiles/UserAchievementProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Kulinarka.DTO;
+using Kulinarka.Models;
+
+namespace Kulinarka.Profiles
+{
+    public class UserAchievementProfile : Profile
+    {
+        public UserAchievementProfile()
+        {
+            CreateMap<UserAchievement, UserAchievementDTO>();
+            CreateMap<UserAchievementDTO, UserAchievement>();
+        }
+    }
+}
