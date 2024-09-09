776d1b4e423c6bb0eff4ce170d2e3112391013f7
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-10 00:32:00 +0200

    Added userTitle repository model and service and implemented logic for updating user title

---
+++ KulinarkaBackend/RepositoryInterfaces/IUserTitleRepository.cs
@@ -0,0 +1,11 @@
+﻿using Kulinarka.Models;
+using Kulinarka.Models.Responses;
+
+namespace Kulinarka.RepositoryInterfaces
+{
+    public interface IUserTitleRepository:IRepository<UserTitle>
+    {
+        public Task<Response<UserTitle>> GetUserTitleEagerAsync(int userId);
+        Task<Response<UserTitle>> UpdateAsync(UserTitle userTitle);
+    }
+}
---
+++ KulinarkaBackend/ServiceInterfaces/IUserTitleService.cs
@@ -0,0 +1,12 @@
+﻿using Kulinarka.Models;
+using Kulinarka.Models.Responses;
+
+namespace Kulinarka.ServiceInterfaces
+{
+    public interface IUserTitleService
+    {
+        public Task<Response<UserTitle>> GetUserTitleEagerAsync(int userId);
+        public Task<Response<UserTitle>> UpdateUserTitle(User user);
+
+    }
+}
