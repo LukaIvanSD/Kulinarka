17404d310794826346dfeb0ea5b032dc26d9d46d
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-09-28 14:43:10 +0200

    Added creating recipe statistics when posting recipe

---
+++ KulinarkaBackend/ServiceInterfaces/IRecipeStatisticsService.cs
@@ -0,0 +1,11 @@
+﻿
+using Kulinarka.Models;
+using Kulinarka.Models.Responses;
+
+namespace Kulinarka.ServiceInterfaces
+{
+    public interface IRecipeStatisticsService
+    {
+        Task<Response<RecipeStatistics>> AddAsync(int recipeId,bool saveChanges=true);
+    }
+}
